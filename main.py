ed0a418c166edd83525ca4806d6687236d21e595
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2022-09-01 22:10:47 +0200

    create web scanning

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import json
 
+import requests
+import os
+
+URL_TO_SCAN = "https://www.ebay-kleinanzeigen.de/"
+
+
+def scanning():
+    response = requests.get('https://www.ebay-kleinanzeigen.de/')
+
+    if not os.path.exists("content.txt"):
+        open("content.txt", 'w+').close()
+
+    filehandle = open("content.txt", 'r')
+    previous_response_html = filehandle.read()
+    filehandle.close()
+
+    if previous_response_html == response.text:
+        return False
+    else:
+        filehandle = open("previous_content.txt", 'w')
+        filehandle.write(response.text)
+        filehandle.close()
+        return True
